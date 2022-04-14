0756bd2dd47e85c9d8517bdb2bfe8c73556e96c4
iwakunibob <totorobob@protonmail.com>
2022-04-14 14:02:51 +0900

    treeshake 8

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-import * as bootstrap from 'bootstrap';
+// import whole bootstap library
+// import * as bootstrap from 'bootstrap';
+
+// import only the required parts for website dependencies
+import { 
+  // Alert, 
+  // Button, 
+  // Carousel, 
+  Collapse, 
+  // Dropdown, 
+  // Modal, 
+  // Offcanvas, 
+  // Popover, 
+  // ScrollSpy, 
+  // Tab, 
+  // Toast, 
+  // Tooltip
+ } from 'bootstrap';
